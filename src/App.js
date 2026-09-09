8a89db1ff9724352042f40f5dc5288706a445b93
Mauricio Ortiz <mauro.itl7@gmail.com>
2026-09-08 22:31:35 -0600

    Primer commit Music Library

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Header from "./components/Header";
+import Song from "./components/Song";
+import "./app.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  componentDidMount() {
+    console.log("La app se ha cargado correctamente");
+  }
+  render() {
+    return (
+      <div>
+        <Header />
+
+        <main>
+          <h2>Mis canciones</h2>
+
+          <Song
+            title="Ameri"
+            artist="Duki"
+            album="Ameri"
+            duration="3:19"
+          />
+
+          <Song
+            title="Goteo"
+            artist="Duki"
+            album="Super Sangre Joven"
+            duration="3:05"
+          />
+
+          <Song
+            title="Antes de Perderte"
+            artist="Duki"
+            album="Antes de Ameri"
+            duration="2:46"
+          />
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
